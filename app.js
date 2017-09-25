b790a6ac005d81b19dd88bcbb4683519e14b09be
RD <richard.thericgroup@hotmail.com>
2017-09-25 21:10:16 +1000

    notes

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
+
+const argv = yargs.argv;
 const notes = require('./notes');
 
-var command = process.argv[2];
+//var command = process.argv[2];
+var command = argv._[0];
 
 console.log(process.argv);
 console.log('Command:', command);
+console.log('Yargs',argv);
 
 if (command === 'add') {
-    console.log('add')
+    //console.log('add')
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('list')
+    //console.log('list')
+    notes.getAll();
+}
+
+switch (command) {
+    case 'add':
+        notes.addNote(argv.title, argv.body);
+        break;
+    case 'list':
+        notes.getAll();
+        break;
+    case 'read':
+        notes.getNote(argv.title);
+        break;
+    case 'remove':
+        notes.removeNote(argv.title);
+        break;
+    default:
+        break;
+    
 }
--- notes.js
+++ notes.js
@@ -1,2 +1,25 @@
 
 console.log('start of notes');
+
+var addNote = (title, body)=>{
+    console.log('adding note',title,body);
+};
+
+var getAll = ()=>{
+    console.log('get all');
+};
+
+var getNote = (title) =>{
+    console.log('get note');
+};
+
+var removeNote = (title)=>{
+    console.log('remove');
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+}
